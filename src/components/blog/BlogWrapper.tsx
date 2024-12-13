5a77e59642c9397dfb0a6c43a1a495fbe496ad66
bermudi <github.igizp@dabg.uk>
2024-12-13 16:27:13 -0600

    Refactor BlogWrapper component and improve blog functionality
    - Split BlogWrapper into BlogWrapper and BlogContent components
    - Implement useBlogPosts hook for fetching blog posts
    - Add loading and error states for blog posts
    - Implement dynamic tag fetching using getAllTags
    - Pass searchQuery and setSearchQuery to BlogSearch component
    - Update BlogTags to use dynamically fetched tags
    - Improve overall component structure and data flow

--- src/components/blog/BlogWrapper.tsx
+++ src/components/blog/BlogWrapper.tsx
@@ -1,20 +1,54 @@
-import React from 'react';
-import { BlogProvider } from './BlogContext';
+import React, { useEffect } from 'react';
+import { BlogProvider, useBlog } from './BlogContext';
 import BlogTags from './BlogTags';
 import BlogGrid from './BlogGrid';
 import BlogSearch from './BlogSearch';
+import { useBlogPosts } from '../../hooks/useBlogPosts';
+import { getAllTags } from '../../utils/blogUtils';
 
-const BlogWrapper: React.FC = () => {
-  const { selectedTag, setSelectedTag } = useBlog();
+const BlogContent: React.FC = () => {
+  const { posts, isLoading, error } = useBlogPosts();
+  const { setAllPosts, searchQuery, setSearchQuery, selectedTag, setSelectedTag } = useBlog();
+  const [tags, setTags] = React.useState<string[]>([]);
+
+  useEffect(() => {
+    if (posts.length > 0) {
+      setAllPosts(posts);
+    }
+  }, [posts, setAllPosts]);
+
+  useEffect(() => {
+    const fetchTags = async () => {
+      const allTags = await getAllTags();
+      setTags(allTags);
+    };
+    fetchTags();
+  }, []);
+
+  if (isLoading) {
+    return <div className="text-center py-12">Cargando entradas...</div>;
+  }
+
+  if (error) {
+    return <div className="text-center py-12 text-red-600">Error loading posts: {error}</div>;
+  }
   return (
-    <BlogProvider>
-      <BlogSearch />
+    <>
+      <BlogSearch onSearch={setSearchQuery} />
       <BlogTags
-        tags={[]} // You need to pass the actual tags here
+        tags={tags}
         selectedTag={selectedTag}
         onTagSelect={setSelectedTag}
       />
       <BlogGrid />
+    </>
+  );
+};
+
+const BlogWrapper: React.FC = () => {
+  return (
+    <BlogProvider>
+      <BlogContent />
     </BlogProvider>
   );
 };
